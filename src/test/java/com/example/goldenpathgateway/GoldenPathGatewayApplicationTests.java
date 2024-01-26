770a62f2cbe18c2d1e173025c53382a4e412f74e
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2024-01-25 23:20:29 -0500

    initialize

---
+++ src/test/java/com/example/goldenpathgateway/GoldenPathGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.goldenpathgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldenPathGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
